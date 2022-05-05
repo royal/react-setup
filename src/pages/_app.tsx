f84c9e1cb05906c14646813388045ea4e090a85b
Sam Royal <samroyal@live.com>
2022-05-05 21:41:15 +0100

    Setup react-query provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
+import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import '../styles/globals.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import type { AppProps } from 'next/app'
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp

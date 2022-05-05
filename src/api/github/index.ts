3f5450aa90238fcb771665979c2e0c36c836797a
Sam Royal <samroyal@live.com>
2022-05-05 22:29:10 +0100

    Load org details

--- src/api/github/index.ts
+++ src/api/github/index.ts
@@ -2,12 +2,19 @@ import type { Organisation, Repository } from './types'
 
 const apiUrl = "https://api.github.com";
 
+const buildRequest = (url: string) => {
+  const request = new Request(url);
+  request.headers.append('Accept', 'application/vnd.github.v3+json');
+
+  return request;
+}
+
 export const getOrganisation = async (organisation: string): Promise<Organisation> => {
-  const response = await fetch(`${apiUrl}/orgs/${organisation}`);
+  const response = await fetch(buildRequest(`${apiUrl}/orgs/${organisation}`));
   return await response.json() as Organisation;
 }
 
 export const getRepositoriesForOrganisation = async (organisation: string): Promise<Repository[]> => {
-  const response = await fetch(`${apiUrl}/orgs/${organisation}/repos`);
+  const response = await fetch(buildRequest(`${apiUrl}/orgs/${organisation}/repos`));
   return await response.json() as Repository[];
 }
--- src/pages/github/[organisation]/index.tsx
+++ src/pages/github/[organisation]/index.tsx
@@ -1,12 +1,15 @@
 import { Layout } from 'components/layout';
+import { useOrganisation } from 'hooks/api/github/useOrganisation';
 import { useRouter } from 'next/router';
 
 const OrganisationPage = () => {
   const router = useRouter();
-  const { organisation } = router.query;
+  const { organisation: org } = router.query;
+
+  let { organisation, repositories } = useOrganisation(org as string);
 
   return (
-    <Layout header={{ title: organisation as string }}>
+    <Layout header={{ title: org as string }}>
 
     </Layout>
   );

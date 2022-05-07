12b931993ebc7acee3d91ec4c65e3d47548e5117
Sam Royal <samroyal@live.com>
2022-05-07 17:50:14 +0100

    Cleanup request builder

--- src/api/github/index.ts
+++ src/api/github/index.ts
@@ -1,25 +1,23 @@
 import type { Organisation, Repository } from './types'
 
-const apiUrl = "https://api.github.com";
-
-const buildRequest = (url: string) => {
-  const request = new Request(url);
+const buildRequest = (endpoint: string) => {
+  const request = new Request(`https://api.github.com/${endpoint}`);
   request.headers.append('Accept', 'application/vnd.github.v3+json');
 
   return request;
 }
 
 export const getOrganisation = async (organisation: string): Promise<Organisation> => {
-  const response = await fetch(buildRequest(`${apiUrl}/orgs/${organisation}`));
+  const response = await fetch(buildRequest(`orgs/${organisation}`));
   return await response.json() as Organisation;
 }
 
 export const getRepositoriesForOrganisation = async (organisation: string): Promise<Repository[]> => {
-  const response = await fetch(buildRequest(`${apiUrl}/orgs/${organisation}/repos`));
+  const response = await fetch(buildRequest(`orgs/${organisation}/repos`));
   return await response.json() as Repository[];
 }
 
 export const getRepository = async (organisation: string, repository: string): Promise<Repository> => {
-  const response = await fetch(buildRequest(`${apiUrl}/repos/${organisation}/${repository}`));
+  const response = await fetch(buildRequest(`repos/${organisation}/${repository}`));
   return await response.json() as Repository;
 }

9b146d665e3aaf3ed0f7de74f5aa521ec317bbf8
Sam Royal <samroyal@live.com>
2022-05-08 15:09:59 +0100

    Update tsconfig paths for /styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-import '../styles/globals.css'
+import 'styles/globals.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
